5b8c4c72f3f13eebff1536c30935a44f75d24890
MadalinaTimofte <UP630129@myport.ac.uk>
2014-02-06 03:10:19 +0000

    Camera

--- ProNote/gen/com/example/pronote/R.java
+++ ProNote/gen/com/example/pronote/R.java
@@ -21,10 +21,18 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_camera=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080007;
+        public static final int btnCapturePicture=0x7f080000;
+        public static final int btnRecordVideo=0x7f080001;
+        public static final int camera_preview=0x7f080004;
+        public static final int imgPreview=0x7f080003;
+        public static final int preview=0x7f080002;
+        public static final int videoPreview=0x7f080005;
+        public static final int video_preview=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,7 +43,9 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int btnCapturePicture=0x7f050002;
+        public static final int btnRecordVideo=0x7f050003;
+        public static final int preview=0x7f050004;
     }
     public static final class style {
         /** 
