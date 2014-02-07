637ba19dc96db6fbec47f90d60a0cf340e843f6a
MadalinaTimofte <UP630129@myport.ac.uk>
2014-02-07 23:30:51 +0000

    Login user account

--- ProNote/gen/com/example/pronote/R.java
+++ ProNote/gen/com/example/pronote/R.java
@@ -25,17 +25,32 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
+        public static final int action_settings=0x7f080013;
         public static final int btnCapturePicture=0x7f080000;
+        public static final int btnLinkToLoginScreen=0x7f080012;
+        public static final int btnLinkToRegisterScreen=0x7f08000c;
+        public static final int btnLogin=0x7f08000b;
+        public static final int btnLogout=0x7f080007;
         public static final int btnRecordVideo=0x7f080001;
+        public static final int btnRegister=0x7f080011;
         public static final int camera_preview=0x7f080004;
         public static final int imgPreview=0x7f080003;
+        public static final int loginEmail=0x7f080008;
+        public static final int loginPassword=0x7f080009;
+        public static final int login_error=0x7f08000a;
         public static final int preview=0x7f080002;
+        public static final int registerEmail=0x7f08000e;
+        public static final int registerName=0x7f08000d;
+        public static final int registerPassword=0x7f08000f;
+        public static final int register_error=0x7f080010;
         public static final int videoPreview=0x7f080005;
         public static final int video_preview=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int dashboard=0x7f030001;
+        public static final int login=0x7f030002;
+        public static final int register=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
